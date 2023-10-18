32bc9bfb5e5e113779b2fc440f1ce1212a1a0d30
unknown <agriadhiatma@gmail.com>
2023-10-19 00:53:00 +0700

    feat: api for getTotalProductCount, getTotalStoreValue and getOutOfStockProducts

--- backend/src/routers/product.routes.js
+++ backend/src/routers/product.routes.js
@@ -11,29 +11,6 @@ const productRouter = express();
 productRouter.get("/products", productController.getAllProducts);
 productRouter.put("/:id", productController.updateProduct);
 productRouter.get("/:id", productController.getOneProduct);
-productRouter.post("/add", productController.insertProductController);
-productRouter.delete("/:id", productController.deleteProductController);
-productRouter.get("/products/count", productController.getTotalProductCount);
-productRouter.get(
-  "/products/store-values",
-  productController.getTotalStoreValue
-);
-productRouter.get(
-  "/products/out-of-stock",
-  productController.getOutOfStockItemsWithCount
-);
-
-productRouter.get("/products/count", productController.getTotalProductCount);
-productRouter.get(
-  "/products/store-values",
-  productController.getTotalStoreValue
-);
-productRouter.get(
-  "/products/out-of-stock",
-  productController.getOutOfStockItemsWithCount
-);
-productRouter.post("/add", productController.insertProductController);
-productRouter.delete("/:id", productController.deleteProductController);
 productRouter.get("/products/count", productController.getTotalProductCount);
 productRouter.get(
   "/products/store-values",
