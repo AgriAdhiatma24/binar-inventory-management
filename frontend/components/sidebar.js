e15ad4e86bf283002edcbe4a0afd232f5c63d7ae
rizkyr99 <ramarizdev@gmail.com>
2023-10-23 03:24:52 +0700

    feat(sidebar): add sidebar item functionality

--- frontend/components/sidebar.js
+++ frontend/components/sidebar.js
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   LayoutGrid,
   Package,
@@ -6,42 +8,62 @@ import {
   User2,
   LogOut,
 } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const items = [
+  {
+    label: 'Dashboard',
+    icon: <LayoutGrid size={20} />,
+    href: '/',
+  },
+  {
+    label: 'Inventory',
+    icon: <Package size={20} />,
+    href: '/inventory',
+  },
+  {
+    label: 'Categories',
+    icon: <Folder size={20} />,
+    href: '/categories',
+  },
+  {
+    label: 'Analytics',
+    icon: <PieChart size={20} />,
+    href: '/analytics',
+  },
+];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside>
-      <div class='top'>
-        <div class='logo'>
+      <div className='top'>
+        <div className='logo'>
           <div className='text-2xl font-bold'>Expenses App</div>
         </div>
-        <div class='close' id='close-btn'>
-          <span class='material-symbols-outlined'> close </span>
+        <div className='close' id='close-btn'>
+          <span className='material-symbols-outlined'> close </span>
         </div>
       </div>
 
-      <div class='sidebar'>
-        <a href='#' class='btn active'>
-          <LayoutGrid size={20} />
-          <h3>Dashboard</h3>
-        </a>
-        <a href='#' class='btn'>
-          <Package size={20} />
-          <h3>Inventory</h3>
-        </a>
-        <a href='#' class='btn'>
-          <Folder size={20} />
-          <h3>Categories</h3>
-        </a>
-        <a href='#' class='btn'>
-          <PieChart size={20} />
-          <h3>Analytics</h3>
-        </a>
-        <div class='sidebar-bottom'>
-          <a href='#' class='btn'>
+      <div className='sidebar'>
+        {items.map((item) => (
+          <Link
+            href={item.href}
+            className={`btn ${pathname === item.href && 'active'}`}>
+            {item.icon}
+            <h3>{item.label}</h3>
+          </Link>
+        ))}
+
+        <div className='sidebar-bottom'>
+          <Link href='/profile' className='btn'>
             <User2 size={20} />
             <h3>Profile</h3>
-          </a>
-          <a href='#' class='btn'>
+          </Link>
+          <a href='#' className='btn'>
             <LogOut size={20} />
             <h3>Logout</h3>
           </a>
