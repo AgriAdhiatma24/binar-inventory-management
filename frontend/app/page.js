197d1e9c374ba27dacb0dc07812f5aa87adb0f14
rizkyr99 <ramarizdev@gmail.com>
2023-10-23 01:32:51 +0700

    feat: dashboard base layout

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import Sidebar from '@/components/sidebar';
+import { Package, DollarSign, Sun, Moon, Plus } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -29,35 +30,29 @@ export default function Home() {
 
         <div className='insight'>
           <div className='balance' id='balance'>
-            <span className='material-symbols-outlined'>
-              account_balance_wallet
+            <span className='dashboard-card-icon'>
+              <Package />
             </span>
             <div className='info' id='info-balance'>
-              <h3>Total Balance</h3>
+              <h3>Total Items</h3>
               <h1>Rp.-10.000</h1>
             </div>
             <small className='text-muted'>Last 30 days</small>
           </div>
           <div className='income' id='income'>
-            <span className='material-symbols-outlined'> north_east </span>
+            <span className='dashboard-card-icon'>
+              <DollarSign />
+            </span>
             <div className='info' id='info-income'>
-              <h3>Total Income</h3>
+              <h3>Total Value</h3>
               <h1>Rp.10.000</h1>
-            </div>
-            <small className='text-muted'>Last 30 days</small>
-          </div>
-          <div className='expenses' id='expenses'>
-            <span className='material-symbols-outlined'> south_west </span>
-            <div className='info' id='info-expenses'>
-              <h3>Total Expenses</h3>
-              <h1>Rp.20.000</h1>
             </div>
             <small className='text-muted'>Last 30 days</small>
           </div>
         </div>
 
         <div id='transaction-table' className='transaction-table'>
-          <h2>List of Transaction</h2>
+          <h2>Recent Activity</h2>
           <table>
             <thead>
               <tr>
@@ -82,11 +77,10 @@ export default function Home() {
           </button>
           <div className='theme-toggler'>
             <span className='material-symbols-outlined active light-mode'>
-              light_mode
+              <Sun size={16} />
             </span>
             <span className='material-symbols-outlined dark-mode'>
-              {' '}
-              dark_mode{' '}
+              <Moon size={16} />
             </span>
           </div>
           <div className='account-info'>
@@ -104,7 +98,9 @@ export default function Home() {
           <h2>New Transaction</h2>
           <div className='add-transaction' id='add-transaction'>
             <a href='form.html'>
-              <span className='material-symbols-outlined'> add </span>
+              <span className='material-symbols-outlined'>
+                <Plus />
+              </span>
               <h3>Add Transaction</h3>
             </a>
           </div>
