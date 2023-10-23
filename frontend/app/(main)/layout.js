e0711815a9435bfee6f62561eb9b32b1fa35de4e
rizkyr99 <ramarizdev@gmail.com>
2023-10-23 14:22:36 +0700

    feat: create navbar component

--- frontend/app/(main)/layout.js
+++ frontend/app/(main)/layout.js
@@ -1,12 +1,16 @@
-import RightSide from '@/components/right-side';
+import Navbar from '@/components/navbar';
 import Sidebar from '@/components/sidebar';
+import RightSide from '@/components/right-side';
 
 export default function MainLayout({ children }) {
   return (
-    <div className='container'>
+    <div className='grid grid-cols-[max-content,1fr] p-8 gap-x-8'>
       <Sidebar />
-      {children}
-      <RightSide />
+      <div>
+        <Navbar />
+        <main className='grid grid-col-[max-content,1fr]'>{children}</main>
+      </div>
+      {/* <RightSide /> */}
     </div>
   );
 }
