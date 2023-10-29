5f6eaaeba747dbcf231843c10e3431f47c8c9a8b
unknown <agriadhiatma@gmail.com>
2023-10-29 22:14:25 +0700

    feat: register page

--- frontend/app/(auth)/register/page.js
+++ frontend/app/(auth)/register/page.js
@@ -1,94 +1,127 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function Register() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [fullname, setFullname] = useState('');
-  const [resp, setResp] = useState({});
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [address, setAddress] = useState({});
 
-  const handleRegister = async () => {
-    var myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-
-    var raw = JSON.stringify({
-      email,
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      username,
       password,
       fullname,
-    });
-
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow',
+      dateOfBirth,
+      address,
     };
 
-    fetch('http://localhost:9000/', requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        setResp(result);
-        navigate('/sign-in');
-      })
-      .catch((error) => console.log('error', error));
+    await axios.post("http://localhost:9000/api/v1/auth/register", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    toast.success("User Registered!.");
+    router.push("/login");
   };
 
   return (
-    <div className='relative flex flex-col items-center justify-center min-h-screen overflow-hidden'>
-      <div className='w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl'>
-        <h1 className='text-3xl font-bold text-center text-gray-700'>
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-bold text-center text-gray-700">
           Binar Inventory Management
         </h1>
-        <h2 className='text-xl font-bold text-center text-gray-700'>
+        <h2 className="text-xl font-bold text-center text-gray-700">
           Register Form
         </h2>
-        <form className='mt-6'>
-          <div className='mb-4'>
+        <form className="mt-6" onSubmit={onSubmit}>
+          <div className="mb-4">
             <label
-              htmlFor='email'
-              className='block text-sm font-semibold text-gray-800'>
-              Masukan email anda
+              htmlFor="username"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Username
             </label>
             <input
-              type='text'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              type="text"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-          <div className='mb-2'>
+          <div className="mb-2">
             <label
-              htmlFor='password'
-              className='block text-sm font-semibold text-gray-800'>
-              Masukan Password Anda
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
             </label>
             <input
-              type='password'
+              type="password"
+              placeholder="Enter password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-          <div className='mb-2'>
+          <div className="mb-2">
             <label
-              htmlFor='text'
-              className='block text-sm font-semibold text-gray-800'>
-              Full Name
+              htmlFor="text"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Fullname
             </label>
             <input
-              type='text'
+              type="text"
+              placeholder="Enter fullname"
               value={fullname}
               onChange={(e) => setFullname(e.target.value)}
-              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-          <div className='mt-2'>
+          <div className="mb-4">
+            <label
+              htmlFor="dateOfBirth"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Date of Birth
+            </label>
+            <input
+              type="date"
+              value={dateOfBirth}
+              onChange={(e) => setDateOfBirth(e.target.value)}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="address"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Address
+            </label>
+            <input
+              type="text"
+              placeholder="Enter address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mt-2">
             <button
-              onClick={handleRegister}
-              className='w-full px-4 mt-6 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600'>
-              Daftar
+              type="submit"
+              className="w-full px-4 mt-6 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+            >
+              Register
             </button>
-            <pre>{JSON.stringify(resp, null, 2)}</pre>
           </div>
         </form>
       </div>
